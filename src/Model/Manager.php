0a0e4db1f8e65028d9bddb38484d3163f910d841
BaNe021200 <32198723+BaNe021200@users.noreply.github.com>
2018-04-24 12:05:33 +0200

    Update Manager.php

--- src/Model/Manager.php
+++ src/Model/Manager.php
@@ -31,7 +31,7 @@ class Manager
      */
     public function __construct($table)
     {
-        $this->pdo = new PDO('mysql:host=localhost;dbname=twig','root','nzB69yCSBDz9eK46',array(
+        $this->pdo = new PDO('mysql:host=localhost;dbname=twig','root','',array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
